a9f2adc18e3bf525332769fb6bd4cb81333c59d1
yeahmatts <mattyeh987@gmail.com>
2015-10-21 08:09:32 -0700

    modified pde

--- Chick.java
+++ Chick.java
@@ -1,4 +1,37 @@
 class Chick implements Animal 
 {     
      //your code here
+	private String myType;
+	private String mySound;
+	public Chick(String type, String sound)
+	{
+		myType = type;
+		mySound = sound;
+	}
+	public Chick(String type, String soundOne, String soundTwo)
+	{
+		myType = type;
+		if (Math.random() < .5)
+		{
+			mySound = soundOne;
+		}
+		else
+		{
+			mySound = soundTwo;
+		}
+	}
+	public Chick()
+	{
+		myType = "unknown";
+		mySound = "unknown";
+	}
+	public String getSound()
+	{
+		return mySound;
+	}
+	public String getType()
+	{
+		return myType;
+	}
+
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -2,5 +2,7 @@
 void setup()
 {
 	//your variable declarations here
-}
+	Farm TheFarm = new Farm();
+	TheFarm.animalSounds();
+};
 
